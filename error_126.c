71784098f7895be8eea1808d5a7436f68190b796
Bruka25 <bkteshome862@gmail.com>
2023-10-18 03:44:02 -0700

    error for permission denied messages

---
+++ error_126.c
@@ -0,0 +1,37 @@
+#include "error.h"
+#include "bk_shell.h"
+
+/**
+ * _error126 - Error message for permission denied failures
+ * 
+ *@args: An array of arguments given to the command
+ *
+ * Return: String of an error
+ */
+char *_error126(char **args)
+{
+        char *error, *hist_str;
+        int len;
+
+        hist_str = _itoa(hist);
+        if (!hist_str)
+                return (NULL);
+
+        len = _strlen(name) + _strlen(hist_str) + _strlen(args[0]) + 24;
+        error = malloc(sizeof(char) * (len + 1));
+        if (!error)
+        {
+                free(hist_str);
+                return (NULL);
+        }
+
+        _strcpy(error, name);
+        _strcat(error, ": ");
+        _strcat(error, hist_str);
+        _strcat(error, ": ");
+        _strcat(error, args[0]);
+        _strcat(error, ": Permission denied\n");
+
+        free(hist_str);
+        return (error);
+}
