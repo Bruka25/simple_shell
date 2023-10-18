8f758ca233f5e05ed2f73ae1d18c3a8e4cf27fba
nati2331 <natnael.mk2@gmail.com>
2023-10-18 13:09:08 +0300

    add node to the end of the list

---
+++ add_node_end.c
@@ -0,0 +1,34 @@
+#include "bk_shell.h"
+/**
+ *addnode_toend - Adds a node to the end of a list_type list
+ *
+ *@head: Pointer to the head of the list_type linked list
+ *@dir: Directory path for the new node to contain
+ *
+ *Return: NULL if error occurs, otherwise a pointer to
+          the new node        
+ */
+
+list_t *addnode_toend(list_t **head, char *dir)
+{
+        list_t *new_node = malloc(sizeof(list_t));
+        list_t *last;
+
+        if (!new_node)
+                return (NULL);
+
+        new_node->dir = dir;
+        new_node->next = NULL;
+
+        if (*head)
+        {
+                last = *head;
+                while (last->next != NULL)
+                        last = last->next;
+                last->next = new_node;
+        }
+        else
+                *head = new_node;
+
+        return (new_node);
+}
